5aa48fb1ad12e6d4504d4fceca42616fadbad20a
srajput45 <43792808+srajput45@users.noreply.github.com>
2020-08-26 20:46:11 +0530

    Updates

---
+++ precache-manifest.e862ba5b9f1bf4607fb7b84e7a2912d2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f98cbd89a183f73bd03be5aef2e75fe",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d990b7c1a2baff2f0c1b",
+    "url": "/portfolio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "bafa570ab38a052b1633",
+    "url": "/portfolio/static/css/main.140b56c5.chunk.css"
+  },
+  {
+    "revision": "d990b7c1a2baff2f0c1b",
+    "url": "/portfolio/static/js/2.8e05ded5.chunk.js"
+  },
+  {
+    "revision": "7694ed5eff56d74dfc4b6ac3fc78a12b",
+    "url": "/portfolio/static/js/2.8e05ded5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bafa570ab38a052b1633",
+    "url": "/portfolio/static/js/main.1bb891fb.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "9db053e0a3c83e27563657076bd8690d",
+    "url": "/portfolio/static/media/Shubham Image.9db053e0.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/man2.52be3144.svg"
+  }
+]);
