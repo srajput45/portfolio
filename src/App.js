dc8fea5a00fe304f89b6654117aa41fa9934397a
srajput45 <43792808+srajput45@users.noreply.github.com>
2020-08-26 21:07:53 +0530

    Fixed the homepage url

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ class App extends Component {
             <Route path="/home" component={Home} />
             <Route path="/project" component={Project} />
             <Route path="/about" component={About} />
-            <Route path="/portfolio" exact component={Home} />
+            <Route path="/portfolio"  component={Home} />
+            <Route path="/" exact component={Home} />
           </Switch>
         </div>
       </React.Fragment>
